245e78df7288a1c50ea627dced63e42afabf228c
TheGameProfi <88748809+TheGameProfi@users.noreply.github.com>
2022-04-05 10:17:00 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-.\gradlew build & copy .\build\libs\modid-1.0.jar C:\Users\Marc\AppData\Roaming\.minecraft\mods & "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
+.\gradlew build & copy .\build\libs\modid-1.0.jar C:\Users\mbg14\AppData\Roaming\.minecraft\mods & "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
